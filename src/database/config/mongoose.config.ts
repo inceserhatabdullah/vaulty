2c683908698186a46bd00f4687309d8622a20401
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-04-03 20:44:01 +0300

    added ibaseentity interface for common properties for all entitis and added mongoose plugin

---
+++ src/database/config/mongoose.config.ts
@@ -0,0 +1,22 @@
+import { Schema, Query } from "mongoose";
+
+export const globalEntityPlugin = (schema: Schema) => {
+  schema.add({
+    _id: { type: String, default: () => crypto.randomUUID() },
+    isDeleted: { type: Boolean, default: false, select: false },
+  });
+
+  schema.set("timestamps", true);
+  schema.set("versionKey", false);
+
+  const findMethods = [
+    "find",
+    "findOne",
+    "findOneAndUpdate",
+    "countDocuments",
+  ] as any;
+
+  schema.pre(findMethods, function (this: Query<any, any>) {
+    this.where({ isDeleted: { $ne: true } });
+  });
+};
