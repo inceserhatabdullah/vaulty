59966992539b65d156aab5e3e1f71281c08957a5
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-04-11 19:53:35 +0300

    update non selectable properties

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { authService } from "../services/auth.service";
+import { sessionService } from "../services/session.service";
 
 export const signup = async (request: Request, response: Response) => {
   try {
@@ -47,6 +48,18 @@ export const logout = async (request: Request, response: Response) => {
   try {
     await authService.logout(request, response);
     return response.status(204).send();
+  } catch (error: any) {
+    return response.status(400).json({ message: error.message });
+  }
+};
+
+export const session = async (request: Request, response: Response) => {
+  try {
+    const user = request.authorization?.user;
+
+    const sessions = await sessionService.find({ userId: user?._id });
+
+    return response.status(200).json(sessions);
   } catch (error: any) {
     return response.status(400).json({ message: error.message });
   }
